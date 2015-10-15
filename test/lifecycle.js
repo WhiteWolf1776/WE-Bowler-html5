7151ca0c45ddfec83acfb529c96e09c2954d1b3d
Nick Blizzard <nick.blizzard@fotlinc.com>
2015-10-15 10:35:40 -0500

    added lifecycle proof of concept test to testing folder

---
+++ test/lifecycle.js
@@ -0,0 +1,19 @@
+describe( "LifeCycleTests", function() {
+
+    beforeEach( function() {
+        console.log( "setup" );
+    } );
+
+    afterEach( function() {
+        console.log( "teardown" );
+    } );
+
+    it( 'test1', function() {
+        console.log( "test1" );
+    } );
+
+    it( 'test2', function() {
+        console.log( "test2" );
+    } );
+
+} );
